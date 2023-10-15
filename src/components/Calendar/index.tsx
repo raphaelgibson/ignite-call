ca954e95661afb578065cc0b9ba7362c491c887e
Raphael Gibson <raphael.araujo@estantemagica.com.br>
2023-10-15 19:03:25 -0300

    initial commit

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,190 @@
+import dayjs from 'dayjs'
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import { useMemo, useState } from 'react'
+import { getWeekDays } from '@/src/utils/get-week-days'
+import {
+  CalendarActions,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from './styles'
+import { useQuery } from '@tanstack/react-query'
+import { api } from '@/src/lib/axios'
+import { useRouter } from 'next/router'
+
+interface CalendarWeek {
+  week: number
+  days: Array<{
+    date: dayjs.Dayjs
+    disabled: boolean
+  }>
+}
+
+type CalendarWeeks = CalendarWeek[]
+
+interface BlockedDates {
+  blockedWeekDays: number[]
+  blockedDates: number[]
+}
+
+interface CalendarProps {
+  onDateSelected: (date: Date) => void
+}
+
+export function Calendar({ onDateSelected }: CalendarProps) {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
+  const router = useRouter()
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, 'month')
+    setCurrentDate(previousMonthDate)
+  }
+
+  function handleNextMonth() {
+    const nextMonthDate = currentDate.add(1, 'month')
+    setCurrentDate(nextMonthDate)
+  }
+
+  const shortWeekDays = getWeekDays({ short: true })
+
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
+
+  const username = String(router.query.username)
+
+  const { data: blockedDates } = useQuery<BlockedDates>(
+    [
+      'blocked-dates',
+      username,
+      currentDate.get('year'),
+      currentDate.get('month'),
+    ],
+    async () => {
+      const response = await api.get(`/users/${username}/blocked-dates`, {
+        params: {
+          year: currentDate.get('year'),
+          month: currentDate.get('month') + 1,
+        },
+      })
+
+      return response.data
+    },
+  )
+
+  const calendarWeeks = useMemo(() => {
+    if (!blockedDates) {
+      return []
+    }
+
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, index) => {
+      const date = currentDate.set('date', index + 1)
+
+      return {
+        date,
+        disabled:
+          date.endOf('day').isBefore(new Date()) ||
+          blockedDates.blockedWeekDays.includes(date.get('day')) ||
+          blockedDates.blockedDates.includes(date.get('date')),
+      }
+    })
+
+    const firstWeekDay = currentDate.get('day')
+
+    const previousMonthFillArray = Array.from({
+      length: firstWeekDay,
+    }).map((_, index) => {
+      return {
+        date: currentDate.subtract(firstWeekDay - index, 'day'),
+        disabled: true,
+      }
+    })
+
+    const lastDayInCurrentMonth = currentDate.set(
+      'date',
+      currentDate.daysInMonth(),
+    )
+    const lastWeekDay = lastDayInCurrentMonth.get('day')
+
+    const nextMonthFillArray = Array.from({
+      length: 7 - (lastWeekDay + 1),
+    }).map((_, index) => {
+      return {
+        date: lastDayInCurrentMonth.add(index + 1, 'day'),
+        disabled: true,
+      }
+    })
+
+    const calendarDays = [
+      ...previousMonthFillArray,
+      ...daysInMonthArray,
+      ...nextMonthFillArray,
+    ]
+
+    const calendarWeeks: CalendarWeeks = []
+
+    for (let i = 0; i < calendarDays.length; i += 7) {
+      calendarWeeks.push({
+        week: i / 7,
+        days: calendarDays.slice(i, i + 7),
+      })
+    }
+
+    return calendarWeeks
+  }, [currentDate, blockedDates])
+
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          {currentMonth} <span>{currentYear}</span>
+        </CalendarTitle>
+
+        <CalendarActions>
+          <button onClick={handlePreviousMonth} title="Previous month">
+            <CaretLeft />
+          </button>
+          <button onClick={handleNextMonth} title="Next month">
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekDay) => (
+              <th key={weekDay}>{weekDay}.</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {calendarWeeks.map(({ week, days }) => {
+            return (
+              <tr key={week}>
+                {days.map(({ date, disabled }) => {
+                  return (
+                    <td key={date.toString()}>
+                      <CalendarDay
+                        onClick={() => onDateSelected(date.toDate())}
+                        disabled={disabled}
+                      >
+                        {date.get('date')}
+                      </CalendarDay>
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
---
+++ src/utils/get-week-days.ts
@@ -0,0 +1,17 @@
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
+  const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
+
+  return Array.from(Array(7).keys()).map((day) => {
+    const weekDay = formatter.format(new Date(Date.UTC(2023, 7, day)))
+
+    if (short) {
+      return weekDay.substring(0, 3).toUpperCase()
+    }
+
+    return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
+  })
+}
